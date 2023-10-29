e8d66bd569a6866753a990c2c58cda8520673222
Pablo Gomez Barreiro <pablogomezbr@hotmail.es>
2023-10-29 21:48:11 +0000

    new report structure

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 
 Extracts information from Hindawi journals [page](https://www.hindawi.com/journals/) on monthly basis to track changes in APC costs, Acceptance rates and other metrics
-Each week is stored in a report on the Data folder
+Each week is stored in a report file on the Data folder
 
--- hindawi_tracker.R
+++ hindawi_tracker.R
@@ -21,8 +21,14 @@ data_values<-data.frame(data=data,values=value)%>%
   mutate(bin_apc2=cumsum(bin_apc))%>%
   mutate(bin_apc2=ifelse(bin_apc2==0,1,bin_apc2))%>%  #put 1s instead of 0s in first rows
   mutate(journal=journal_title[bin_apc2])%>%
-  select(journal, data, values,-bin_apc,-bin_apc2)
+  select(journal, data, values,-bin_apc,-bin_apc2)%>%
+  mutate(date_stamp=Sys.Date())
+
+url<-"https://github.com/pgomba/Hindawi_tracker/blob/main/data/report.csv"
+original_data<-read.csv(url(url))
 
 dir.create("data") 
 
-write.csv(data_values,row.names = FALSE,paste0("data/report",Sys.Date(),".csv"))
+data_values<-bind_rows(original_data,data_values)
+
+write.csv(data_values,row.names = FALSE,paste0("data/report.csv"))
